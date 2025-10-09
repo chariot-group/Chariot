ece0281f9cf7da566ba79503190bad4027c5ea5f
= <=>
2025-10-09 22:57:10 +0200

    stp

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,28 +6,33 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '@/resources/user/user.module';
 import { GroupModule } from '@/resources/group/group.module';
 import { CampaignModule } from '@/resources/campaign/campaign.module';
-import { SeederModule } from '@/seeder/seeder.module';
 import { AuthModule } from '@/resources/auth/auth.module';
 import { MaillingService } from '@/mailling/mailling.service';
 import { MaillingModule } from '@/mailling/mailling.module';
 import { CharacterModule } from '@/resources/character/character.module';
 import { StripeModule } from '@/resources/stripe/stripe.module';
 
+const imports = [
+  ConfigModule.forRoot({
+    isGlobal: true, // Permet d'accéder aux variables du env dans tous les modules
+  }),
+  MongooseModule.forRoot(process.env.MONGO_URL),
+  UserModule,
+  CharacterModule,
+  GroupModule,
+  CampaignModule,
+  AuthModule,
+  MaillingModule,
+  StripeModule,
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { SeederModule } = require('@/seeder/seeder.module');
+  imports.push(SeederModule);
+}
+
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Permet d'accéder aux variables du env dans tous les modules
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URL),
-    UserModule,
-    CharacterModule,
-    GroupModule,
-    CampaignModule,
-    SeederModule,
-    AuthModule,
-    MaillingModule,
-    StripeModule,
-  ],
+  imports,
   controllers: [AppController],
   providers: [AppService, Logger, MaillingService],
 })
