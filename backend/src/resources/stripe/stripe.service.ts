106a5af3a7671c322fae6ffb00219d0eef084a95
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-10-10 11:52:04 +0200

    Update packages (#444)
    * Update packages
    
    * change api stripe
    
    ---------
    
    Co-authored-by: = <=>

--- backend/src/resources/stripe/stripe.service.ts
+++ backend/src/resources/stripe/stripe.service.ts
@@ -16,7 +16,7 @@ export class StripeService {
     private readonly logger = new Logger(this.SERVICE_NAME);
 
     private stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: '2025-05-28.basil',
+        apiVersion: '2025-08-27.basil',
     });
 
     /**
