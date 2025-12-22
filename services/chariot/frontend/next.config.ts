a59a26d5a8756dc51255d401bbd656ef6d0d1157
Hugo Piedanna <43523184+hugo-piedanna@users.noreply.github.com>
2025-12-22 13:05:55 +0100

    Chores/clean keycloak (#655)
    * Clean all useless files and optimized config for prod & integ
    
    * fix compose
    
    * Fix compose
    
    * Fix docker file
    
    * Test env
    
    * Add good init var
    
    * Test
    
    * env runtime
    
    * clean
    
    * Version dockerfile
    
    * Optimise dockerfile
    
    * Fix Dockerfile back and deploy.yml
    
    * Fix Dockerfile back and deploy.yml
    
    * Fix access
    
    ---------
    
    Co-authored-by: = <=>

--- services/chariot/frontend/next.config.ts
+++ services/chariot/frontend/next.config.ts
@@ -12,18 +12,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
 
-  // Variables runtime (côté serveur uniquement)
-  serverRuntimeConfig: {
-    nodeEnv: process.env.NODE_ENV,
-  },
-
-  // Variables publiques (exposées au client)
-  publicRuntimeConfig: {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL,
-    keycloakUrl: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
-    keycloakRealm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
-    keycloakClientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
-  },
 };
 
 export default withNextIntl(nextConfig);
