e4336d003b1a0bed2ef33ff0341085cf08fb8baa
= <=>
2026-02-02 17:27:40 +0100

    Fix imports

--- services/adventure/api/src/resources/user/user.module.ts
+++ services/adventure/api/src/resources/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { KeycloakService } from './keycloak.service';
+import { UserService } from '@/resources/user/user.service';
+import { UserController } from '@/resources/user/user.controller';
+import { KeycloakService } from '@/resources/user/keycloak.service';
 
 @Module({
   controllers: [UserController],
