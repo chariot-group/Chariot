6319f5de3d625c288114575c69e996a2532f659b
Eyvos <elvis.pichou@gmail.com>
2026-02-05 14:42:07 +0100

    ✨ Feat: update character battle tab layout and health bar values

--- services/web/client/src/components/character/tabContents/CharacterBattleTabContent.tsx
+++ services/web/client/src/components/character/tabContents/CharacterBattleTabContent.tsx
@@ -20,35 +20,35 @@ const CharacterBattleTabContent = ({ character, accentColor }: Props) => {
                         Statistiques Principales
                     </h2>
                     <div className="flex flex-row justify-start gap-2 text-xl font-extrabold">
-                        <span className="bg-white text-black flex flex-row rounded-full px-3 py-1 gap-2">
+                        <div className="bg-white text-black flex flex-row justify-center gap-1 rounded-full px-5 py-1 items-center">
                             <Image
                                 src={ShieldIcon}
                                 alt=""
                                 aria-hidden="true"
-                                className="size-6 sm:size-7"
+                                className="size-5"
                             />
                             <span aria-hidden="true">18</span>
-                        </span>
-                        <span className="bg-white text-black flex flex-row rounded-full px-3 py-1 gap-2">
+                        </div>
+                        <div className="bg-white text-black flex flex-row justify-center gap-1 rounded-full px-5 py-1 items-center">
                             <Image
                                 src={FeatherIcon}
                                 alt=""
                                 aria-hidden="true"
-                                className="size-6 sm:size-7"
+                                className="size-6"
                             />
                             <span aria-hidden="true">+0</span>
-                        </span>
-                        <span className="bg-white text-black flex flex-row rounded-full px-3 py-1 gap-2">
+                        </div>
+                        <div className="bg-white text-black flex flex-row justify-center gap-1 rounded-full px-5 py-1 items-center">
                             <Image
                                 src={RunningIcon}
                                 alt=""
                                 aria-hidden="true"
-                                className="size-6 sm:size-7"
+                                className="size-6"
                             />
                             <span aria-hidden="true">9m</span>
-                        </span>
+                        </div>
                     </div>
-                    <CharacterHealthBar currentHP={0} maxHP={40} tempHP={0} />
+                    <CharacterHealthBar currentHP={24} maxHP={40} tempHP={8} />
                 </Card >
                 <Card className='gap-2'>
                     <h2 className={`text-2xl sm:text-3xl font-semibold ${accentColor}`}>
