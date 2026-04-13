7d7a40b27150eed5493aca556c13ab7131679bc5
Eyvos <elvis.pichou@gmail.com>
2026-04-13 19:38:00 +0200

    💄 UI(Header, Layout): add app version display in the header and layout for better user awareness

--- services/web/client/src/app/[locale]/layout.tsx
+++ services/web/client/src/app/[locale]/layout.tsx
@@ -44,6 +44,7 @@ export default async function RootLayout({
 }>) {
   const { locale } = await params;
   const messages = await getMessages();
+  const appVersion = process.env.NEXT_PUBLIC_APP_VERSION;
 
   return (
     <html lang={locale}>
@@ -55,13 +56,16 @@ export default async function RootLayout({
               <SidebarProvider>
                 <AppSidebar />
 
-                <div className="flex w-full flex-col h-screen overflow-hidden">
+                <div className="flex w-full flex-col h-screen overflow-hidden relative">
                   <PostLoginNavigator />
                   <LocaleDetector />
                   <ToastContainer />
                   <Header />
 
                   {children}
+                  {appVersion && (
+                    <p className="absolute bottom-0 left-0 lg:inset-x-0 text-center text-[10px] text-white/55 select-none">Chariot v{appVersion}</p>
+                  )}
                 </div>
               </SidebarProvider>
             </ReduxProvider>
