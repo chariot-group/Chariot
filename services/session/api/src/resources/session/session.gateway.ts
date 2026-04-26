14d048d426075d65b64428a305ee760edadcea57
= <=>
2026-04-26 21:15:13 +0200

    Add log

--- services/session/api/src/resources/session/session.gateway.ts
+++ services/session/api/src/resources/session/session.gateway.ts
@@ -56,7 +56,7 @@ export class SessionGateway implements OnGatewayInit, OnGatewayConnection, OnGat
         this.keycloakExternalUrl = this.configService.get<string>('KEYCLOAK_URL');
         this.realm = this.configService.get<string>('KEYCLOAK_REALM');
 
-        const jwksUri = `${this.keycloakInternalUrl}/realms/${this.realm}/protocol/openid-connect/certs`;
+        const jwksUri = `${this.keycloakExternalUrl}/realms/${this.realm}/protocol/openid-connect/certs`;
 
         this.jwksClient = jwksRsa({
             jwksUri,
@@ -406,6 +406,14 @@ export class SessionGateway implements OnGatewayInit, OnGatewayConnection, OnGat
                 this.logger.error(message, null, this.SERVICE_NAME);
                 return reject(new InternalServerErrorException(message));
             }
+
+
+            // Ajoute ça temporairement
+            this.jwksClient.getKeys().then(keys => {
+                this.logger.debug(`Available keys from JWKS endpoint: ${(keys as any).map(k => k.kid).join(', ')}`, this.SERVICE_NAME);
+            }).catch(err => {
+                this.logger.error(`Error fetching keys from JWKS endpoint: ${err.message}`, err.stack, this.SERVICE_NAME);
+            });
 
             this.jwksClient.getSigningKey(kid, (err, key) => {
                 if (err) {
