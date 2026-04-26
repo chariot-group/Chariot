96e87a163df53e86e2f47993b48f5457bbe5f3e6
Eyvos <elvis.pichou@gmail.com>
2026-04-26 10:42:20 +0200

    🐛 Hitfix: update DATABASE_URL handling in PrismaService and CI configuration; bump version to 1.0.1

--- services/session/api/src/prisma/prisma.service.ts
+++ services/session/api/src/prisma/prisma.service.ts
@@ -3,22 +3,28 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy {
     private readonly logger = new Logger(PrismaService.name);
-    private readonly SERVICE_NAME = PrismaService.name;
 
     constructor() {
-        const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+        const connectionString =
+            process.env.DATABASE_URL ??
+            'postgresql://fake:fake@localhost:5432/fake';
+
+        const adapter = new PrismaPg({ connectionString });
+
         super({ adapter });
     }
 
     async onModuleInit() {
         await this.$connect();
-        this.logger.verbose('Connected to PostgreSQL', this.SERVICE_NAME);
+        this.logger.verbose('Connected to PostgreSQL');
     }
 
     async onModuleDestroy() {
         await this.$disconnect();
-        this.logger.verbose('Disconnected from PostgreSQL', this.SERVICE_NAME);
+        this.logger.verbose('Disconnected from PostgreSQL');
     }
 }
